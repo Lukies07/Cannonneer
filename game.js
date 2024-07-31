77ef9fd02a8a5eb489c7f12711c11f3bf384f503
Lukies07 <luke.cillekens@sbhsstudent.school.nz>
2024-08-01 11:02:59 +1200

    added basic javascript canvas and made css changes

--- game.js
+++ game.js
@@ -1,1 +1,13 @@
-can
+document.addEventListener('DOMContentLoaded'), (event) => {
+    console.log('DOM fully loaded and parsed');
+    let canvas = document.addEventListener('gameCanvas')
+    let ctx = canvas.getContext('2d') 
+
+    function resizeCanvas() { 
+        canvas.wdith = window.innerWidth;
+        canvas.height = window.innerHeight;
+    }
+    
+    window.addEventListener('resize', resizeCanvas);
+    resizeCanvas();
+}
